5ce947941c8c4ac31238543b22ccbd949b66bac4
Utshabosu0 <utshabosu007@gmail.com>
2022-06-10 14:44:39 +0600

    home page

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = (props) => {
+    const { cart } = props;
+    const totalReducer = (previous, Product) => previous + Product.price
+    const total = cart.reduce(totalReducer, 0)
+
+
+    // let total = 0;
+    // for (const product of cart) {
+    //     total = total + product.price;
+    // }
+
+    return (
+        <div>
+            <h3>Order Summary</h3>
+            <h5>Items Ordered: {props.cart.length}</h5>
+            <p>Total: {total.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
